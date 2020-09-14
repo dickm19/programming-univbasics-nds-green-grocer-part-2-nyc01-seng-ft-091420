9285ad86af861dd6debe36a147b48c605840a13f
Mary Dick <maryclairedick@gmail.com>
2020-09-14 01:21:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,29 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  coupon_item = []
+  count = 0
+  cart.each do |item|
+    #binding.pry
+    
+    coupons.each do |coupon|
+      binding.pry
+      if item[:item] == coupon[:item]
+        coupon_item << item
+        item[:count] = (item[:count] - coupon[:num])
+        while count < coupon_item.count do
+          coupon_item[count][:price] = coupon[:cost]/coupon[:num]
+          coupon_item[count][:item] = "#{coupon[:item]} W/COUPON"
+          cart << coupon_item
+          count += 1
+        end
+      end
+    end
+    #binding.pry
+  end
+  binding.pry
+  
 end
 
 def apply_clearance(cart)
